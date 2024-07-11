1eda469cc23dfbf632c91814f0eab77fc25ad0b9
gootyboy <gootyboy@icloud.com>
2024-07-11 19:22:37 -0400

    multiplayer coin collector

--- pygame_game/coin_collector/coin.py
+++ pygame_game/coin_collector/coin.py
@@ -4,21 +4,26 @@ from random import randint
 
 WIDTH = 700
 HEIGHT = 600
-score = 0
+fox_score = 0
+hedgehog_score = 0
 game_over = False
 
 fox = Actor("fox")
 fox.pos = 100, 100
+hedgehog = Actor("hedgehog")
+hedgehog.pos = 400, 400
 coin = Actor("coin")
 coin.pos = 200, 200
 
-speed = 10
+speed = 2
 
 def draw():
     screen.fill("green")
     fox.draw()
     coin.draw()
-    screen.draw.text("Score: " + str(score), color="black", topleft =(10, 10))
+    hedgehog.draw()
+    screen.draw.text("Fox score: " + str(fox_score), color="black", topleft =(10, 10))
+    screen.draw.text("Hedgehog score: " + str(hedgehog_score), color="black", topleft =(10, 30))
 
     if game_over:
         screen.clear()
@@ -35,6 +40,8 @@ def time_up():
     game_over = True
 
 def update():
+    global hedgehog_score, fox_score
+
     if keyboard.left:
         fox.x -= speed
     elif keyboard.right:
@@ -44,7 +51,27 @@ def update():
     elif keyboard.down:
         fox.y += speed
 
-clock.schedule(time_up, 10.0)
+    if keyboard.a:
+        hedgehog.x -= speed
+    elif keyboard.d:
+        hedgehog.x += speed
+    elif keyboard.w:
+        hedgehog.y -= speed
+    elif keyboard.s:
+        hedgehog.y += speed
+
+    fox_coin_collected = fox.colliderect(coin)
+    hedgehog_coin_collected = hedgehog.colliderect(coin)
+
+    if fox_coin_collected:
+        fox_score += 10
+        place_coin()
+    
+    if hedgehog_coin_collected:
+        hedgehog_score += 10
+        place_coin()
+
+# clock.schedule(time_up, 10.0)
 place_coin()
 
 pgzrun.go()
