ba94c3c25877b946667925ea975116fcd13edd5a
gootyboy <gootyboy@icloud.com>
2024-07-11 20:19:03 -0400

    multiplayer changes

--- pygame_game/coin_collector/coin.py
+++ pygame_game/coin_collector/coin.py
@@ -1,4 +1,5 @@
 import pgzrun
+import time
 from pgzero.actor import Actor
 from random import randint
 
@@ -7,6 +8,7 @@ HEIGHT = 600
 fox_score = 0
 hedgehog_score = 0
 game_over = False
+player_speed = 10
 
 fox = Actor("fox")
 fox.pos = 100, 100
@@ -15,9 +17,8 @@ hedgehog.pos = 400, 400
 coin = Actor("coin")
 coin.pos = 200, 200
 
-speed = 2
-
 def draw():
+
     screen.fill("green")
     fox.draw()
     coin.draw()
@@ -28,7 +29,8 @@ def draw():
     if game_over:
         screen.clear()
         screen.fill("green")
-        screen.draw.text("Final Score: " + str(score), color= "black", center = ((WIDTH / 2), 50), fontsize = 60)
+        screen.draw.text("Fox Score: " + str(fox_score), color= "black", center = ((WIDTH / 2), 50), fontsize = 60)
+        screen.draw.text("Hedgehog Score: " + str(hedgehog_score), color= "black", center = ((WIDTH / 2), 100), fontsize = 60)
 
 def place_coin():
     coin.x = randint(20, (WIDTH - 20))
@@ -42,23 +44,32 @@ def time_up():
 def update():
     global hedgehog_score, fox_score
 
-    if keyboard.left:
-        fox.x -= speed
-    elif keyboard.right:
-        fox.x += speed
-    elif keyboard.up:
-        fox.y -= speed
-    elif keyboard.down:
-        fox.y += speed
+    if fox.x > 20:
+        if keyboard.left:
+            fox.x -= player_speed
+    if fox.x < (WIDTH -20):
+        if keyboard.right:
+            fox.x += player_speed
+    if fox.y > 20:
+        if keyboard.up:
+            fox.y -= player_speed
+    if fox.y < (HEIGHT -20):
+        if keyboard.down:
+            fox.y += player_speed
 
-    if keyboard.a:
-        hedgehog.x -= speed
-    elif keyboard.d:
-        hedgehog.x += speed
-    elif keyboard.w:
-        hedgehog.y -= speed
-    elif keyboard.s:
-        hedgehog.y += speed
+    if hedgehog.x > 20:
+        if keyboard.a:
+            hedgehog.x -= player_speed
+    if hedgehog.x < (WIDTH -20):
+        if keyboard.d:
+            hedgehog.x += player_speed
+    if hedgehog.y > 20:
+        if keyboard.w:
+            hedgehog.y -= player_speed
+    if hedgehog.y < (HEIGHT -20):
+        if keyboard.s:
+            hedgehog.y += player_speed
+
 
     fox_coin_collected = fox.colliderect(coin)
     hedgehog_coin_collected = hedgehog.colliderect(coin)
@@ -71,7 +82,7 @@ def update():
         hedgehog_score += 10
         place_coin()
 
-# clock.schedule(time_up, 10.0)
+clock.schedule(time_up, 60.0)
 place_coin()
 
 pgzrun.go()
