cdf58d0d7dac84dfffd85c35322c0ba32fec4974
gootyboy <gootyboy@icloud.com>
2024-07-26 09:29:18 -0400

    computer game password encoding and decoding

--- pygame_game/computer-game/computer.py
+++ pygame_game/computer-game/computer.py
@@ -7,17 +7,23 @@ WIDTH = 800
 HEIGHT = 600
 
 box_index = 0
-screen_clicked = False
+settings_width = 451
+settings_height = 451
+settings_x = 70
+settings_y = 76
+
 key_pressed = None
+icon_clicked = None
+
+screen_clicked = False
 password_done = False
 digit_correct_list = [False, False, False, False, False, False, False, False]
 on_home_page = False
 setting_rect_open = False
 windows_rect_open = False
 backround_icon_open = False
-icon_clicked = None
+settings_rect_full_screen = False
 
-settings_rect = pygame.Rect(70, 76, 451, 451)
 windows_rect = pygame.Rect(70, 200, 351, 312)
 
 setting_icon = Actor("setting-icon", pos=(450, 570))
@@ -32,6 +38,10 @@ x_button_1 = Actor("x-button", pos=(500, 93))
 
 x_button_2 = Actor("x-button", pos=(405, 210))
 
+full_screen_icon_1 = Actor("full-screen-icon", pos=(465, 93))
+
+full_screen_icon_2 = Actor("full-screen-icon", pos=(365, 210))
+
 garden_icon = Actor("garden-background-icon", pos=(400, 250))
 
 sky_icon = Actor("sky-background-icon", pos=(200, 250))
@@ -40,12 +50,81 @@ dungeon_icon = Actor("dungeon-background-icon", pos=(200, 400))
 
 stage_icon = Actor("stage-background-icon", pos=(400, 400))
 
+password_coding = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5,
+    "f": 6,
+    "g": 7,
+    "h": 8,
+    "i": 9,
+    "j": 10,
+    "k": 11,
+    "l": 12,
+    "m": 13,
+    "n": 14,
+    "o": 15,
+    "p": 16,
+    "q": 17,
+    "r": 18,
+    "s": 19,
+    "t": 20,
+    "u": 21,
+    "v": 22,
+    "w": 23,
+    "x": 24,
+    "y": 25,
+    "z": 26
+}
+
+def get_letter_from_number(digits, index_of_item):
+    for letter, number in password_coding.items():
+        if str(number) == digits[index_of_item]:
+            return letter
+
+def decode_password(digits):
+    digit_1 = get_letter_from_number(digits, 0)
+    digit_2 = get_letter_from_number(digits, 1)
+    digit_3 = get_letter_from_number(digits, 2)
+    digit_4 = get_letter_from_number(digits, 3)
+    digit_5 = get_letter_from_number(digits, 4)
+    digit_6 = get_letter_from_number(digits, 5)
+    digit_7 = get_letter_from_number(digits, 6)
+    digit_8 = get_letter_from_number(digits, 7)
+    return [digit_1, digit_2, digit_3, digit_4, digit_5, digit_6, digit_7, digit_8]
+
+def check_if_digits_are_coded(digits):
+    try:
+        for digit in digits:
+            digit_int = int(digit)
+        return True
+    except ValueError:
+        return False
+
+def make_password_coded(digits):
+    coded_digits_1_to_4 = [password_coding[digits[0]], password_coding[digits[1]], password_coding[digits[2]],password_coding[digits[3]]]
+    coded_digits_5_to_8 = [password_coding[digits[4]], password_coding[digits[5]], password_coding[digits[6]], password_coding[digits[7]]]
+    coded_digits = coded_digits_1_to_4 + coded_digits_5_to_8
+    filename = r"C:\Projects\boy\pygame_game\computer-game\password.txt"
+    with open(filename, "w") as file:
+        for coded_digit in coded_digits:
+            file.write(str(coded_digit) + " ")
+
 def get_password_digits():
     filename = r"C:\Projects\boy\pygame_game\computer-game\password.txt"
     with open(filename, "r") as file:
         line = file.readline()
-        password_digits = line.split()
-        return password_digits
+        password_digits_coded = line.split()
+        coded = check_if_digits_are_coded(password_digits_coded)
+        if coded:
+            password_digits = decode_password(password_digits_coded)
+            return password_digits
+        else:
+            password_digits = password_digits_coded
+            make_password_coded(password_digits)
+            return password_digits
 
 def get_backround():
     global backround_image,blurred_backround_image
@@ -95,10 +174,13 @@ def draw_home_screen():
     windows_icon.draw()
 
 def start_home_page():
+    global settings_rect_full_screen
     draw_home_screen()
     if setting_rect_open:
         screen.draw.filled_rect(settings_rect, (239,244,249))
         x_button_1.draw()
+        if not settings_rect_full_screen:
+            full_screen_icon_1.draw()
         if backround_icon_open:
             backround_icon.draw()
         elif not backround_icon_open and icon_clicked == "background":
@@ -111,10 +193,20 @@ def start_home_page():
         screen.draw.filled_rect(windows_rect, "gray")
         x_button_2.draw()
         power_off.draw()
+        full_screen_icon_2.draw()
+
+def rect_full_screen(rect):
+    global settings_width, settings_height, settings_x, settings_y
+    if rect == "settings":
+        settings_width = WIDTH
+        settings_height = HEIGHT - 50
+        settings_x = 0
+        settings_y = 0
 
 def draw():
     global screen_clicked, box_index, key_pressed, password_boxes_x,digit_correct_list, on_home_page, password_done, setting_rect_open, backround_icon_open
-
+    global settings_rect, settings_x, settings_y, settings_width, settings_height
+    settings_rect = pygame.Rect(settings_x, settings_x, settings_width, settings_height)    
     get_backround()
     password_digits = get_password_digits()
     time_now = time.localtime()
@@ -148,7 +240,6 @@ def draw():
         password_done = False
         box_index = 0
         digit_correct_list = [False, False, False, False, False, False, False, False]
-    
     if on_home_page:
         start_home_page()
                                 
@@ -175,28 +266,37 @@ def check_for_background_pressed(pos):
         update_background("stage")
         icon_clicked = None
 
+def check_for_other_icons_pressed(pos):
+    global on_home_page, setting_rect_open, backround_icon_open, backround_image, icon_clicked, windows_rect_open, settings_rect_full_screen
+    if setting_icon.collidepoint(pos):
+        setting_rect_open = True
+    if windows_icon.collidepoint(pos):
+        windows_rect_open = True
+    if x_button_1.collidepoint(pos):
+        setting_rect_open = False
+        backround_icon_open = False
+    if x_button_2.collidepoint(pos):
+        windows_rect_open = False
+    if power_off.collidepoint(pos) and windows_rect_open:
+        pgzrun.sys.exit()
+    if full_screen_icon_1.collidepoint(pos) and setting_rect_open:
+        rect_full_screen("settings")
+        settings_rect_full_screen = True
+    if full_screen_icon_1.collidepoint(pos) and windows_rect_open:
+        pass
+    
 def on_mouse_down(pos, button):
     global screen_clicked, on_home_page, setting_rect_open, backround_icon_open, backround_image, icon_clicked, windows_rect_open
     if on_home_page == False:
         screen_clicked = True
     elif on_home_page and button == mouse.LEFT:
-        if setting_icon.collidepoint(pos):
-            setting_rect_open = True
-        if x_button_1.collidepoint(pos):
-            setting_rect_open = False
-            backround_icon_open = False
         if backround_icon.collidepoint(pos) and backround_icon_open:
             backround_icon_open = False
             icon_clicked = "background"
         else:
             backround_icon_open = True
         check_for_background_pressed(pos)
-        if windows_icon.collidepoint(pos):
-            windows_rect_open = True
-        if x_button_2.collidepoint(pos):
-            windows_rect_open = False
-        if power_off.collidepoint(pos) and windows_rect_open:
-            pgzrun.sys.exit()
+        check_for_other_icons_pressed(pos)
 
 def get_letter_pressed():
     if keyboard.q:
