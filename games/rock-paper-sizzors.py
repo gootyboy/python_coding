c59cc757e1506d439b8bde6e2d8e8dcc11425c6c
gootyboy <gootyboy@icloud.com>
2024-08-12 11:24:39 -0400

    rock paper sizzors game update 2 players or 1 player and computer player

--- games/rock-paper-sizzors.py
+++ games/rock-paper-sizzors.py
@@ -1,30 +1,107 @@
 import random
+import sys
+
+def get_and_clear_input(text):
+    while True:
+        user_input = input(text)
+        if check_if_correct_input(user_input, ["rock", "paper", "scissors"]):
+            break
+        else:
+            sys.stdout.write('\033[F')
+            sys.stdout.write('\033[K')
+            sys.stdout.flush()
+            print("You didn't enter \"rock\", \"paper\", or \"scissors\"")
+            print(" ")
+            continue
+    sys.stdout.write('\033[F')
+    sys.stdout.write('\033[K')
+    sys.stdout.flush()
+    return user_input
+
+def check_winner(player_1_move, player_2_move, player_1_name, player_2_name):
+    if player_1_move == player_2_move:
+        return "Tie"
+    elif (player_1_move == "rock" and player_2_move == "scissors") or (player_1_move == "scissors" and player_2_move == "paper") or (player_1_move == "paper" and player_2_move == "rock"):
+        return f"{player_1_name} won. {player_2_name} lost"
+    else:
+        return f"{player_2_name} won. {player_1_name} lost"
+
+def print_winner():
+    global player_1_move, computer_player, player_2_move, player_1_name, player_2_name, computer_move
+    try:
+        if computer_player == "no":
+            print(f"{player_1_name} ---> {player_1_move}")
+            print(f"{player_2_name} ---> {player_2_move}")
+            winner = check_winner(player_1_move, player_2_move, player_1_name, player_2_name)
+        else:
+            winner = check_winner(player_1_move, computer_move, player_1_name, "computer")
+        print(winner)
+    except KeyboardInterrupt:
+        print("Keyboard Interrupt. Game Over")
+        print(" ")
+        ask_play_again()
+
+def ask_play_again():
+    while True:
+        try:
+            while True:
+                play_again = input("Would you like to play again? Enter \"yes\" or \"no\": ")
+                if check_if_correct_input(play_again, ["yes", "no"]):
+                    break
+                else:
+                    print("You didn't enter \"yes\" or \"no\"")
+                    print(" ")
+                    continue
+            print(" ")
+            return play_again
+        except KeyboardInterrupt:
+            print("Keyboard Interrupt. Game Over")
+            print(" ")
+            continue
+
+def check_if_correct_input(input, correct_inputs):
+    for correct_input in correct_inputs:
+        if input.lower() == correct_input:
+            return True
+    else:
+        return False
 
 while True:
-    print("rock, paper, sizzors!")
-    print(" ")
-    move = input("Enter \"rock\", \"paper\", or \"sizzors\". INPUT SHOULD BE EXACTLY HOW IT IS WRITTEN")
-    computer_move = random.choice(["rock", "paper", "sizzors"])
-    print(" ")
-    print(f"computer move: {computer_move}")
-    if move == "rock":
-        if computer_move == "paper":
-            print("You lost. Computer won")
-        elif computer_move == "sizzors":
-            print("You won. Computer lost")
+    try:
+        print("rock, paper, scissors!")
+        print(" ")
+        while True:
+            computer_player = input("Would you like to play with a computer? Enter \"yes\" or \"no\": ")
+            if check_if_correct_input(computer_player, ["yes", "no"]):
+                break
+            else:
+                print("You didn't enter \"yes\", or \"no\"")
+                print(" ")
+                continue
+        print(" ")
+        player_1_name = input("Enter your name. You will be player one. If no name is given, your name will be \"player one\": ")
+        if not player_1_name.strip():
+            player_1_name = "player one"
+        if computer_player == "no":
+            player_2_name = input("Enter your name. You will be player two. If no name is given, your name will be \"player two\": ")
+            if not player_2_name.strip():
+                player_2_name = "player two"
+            player_1_move = get_and_clear_input("Enter \"rock\", \"paper\", or \"scissors\": ")
         else:
-            print("Tie")
-    elif move == "paper":
-        if computer_move == "sizzors":
-            print("You lost. Computer won")
-        elif computer_move == "rock":
-            print("You won. Computer lost")
+            player_1_move = input(f"{player_1_name}'s turn. Enter \"rock\", \"paper\", or \"scissors\": ")
+
+        if computer_player == "yes":
+            computer_move = random.choice(["rock", "paper", "scissors"])
+            print(f"Computer move: {computer_move}")
         else:
-            print("Tie")
-    else:
-        if computer_move == "rock":
-            print("You lost. Computer won")
-        elif computer_move == "paper":
-            print("You won. Computer lost")
-        else:
-            print("Tie")
+            player_2_move = get_and_clear_input(f"{player_2_name}'s turn. Enter \"rock\", \"paper\", or \"scissors\": ")
+        print(" ")
+        print_winner()
+        break_loop = ask_play_again()
+        if break_loop.lower() == "no":
+            break
+    except KeyboardInterrupt:
+        print("Keyboard Interrupt. Game Over")
+        break_loop = ask_play_again()
+        if break_loop.lower() == "no":
+            break
--- pygame_game/sleeping-dragons/dragons.py
+++ pygame_game/sleeping-dragons/dragons.py
@@ -6,33 +6,45 @@ from pgzero.animation import animate
 from pgzero.actor import Actor
 from pgzero.keyboard import keyboard
 
-WIDTH = 2400
+WIDTH = 900
 HEIGHT = 600
+
+EGG_TARGET = 1000
+ATTACK_DISTANCE = 200
+GOLDEN_EGG_HIDE_TIME = 60
+FREEZE_TIME = 10
+DRAGON_AWAKE_TIME = 10
+HERO_SPEED = 5
+EGG_HIDE_TIME = 5
+
 CENTER_X = WIDTH / 2
 CENTER_Y = HEIGHT / 2
+
 CENTER = (CENTER_X, CENTER_Y)
 FONT_COLOR = (0, 0, 0)
-EGG_TARGET = 1000
 HERO_START = (200, 300)
-ATTACK_DISTANCE = 200
-DRAGON_AWAKE_TIME = 10
-EGG_HIDE_TIME = 5
-MOVE_DISTANCE = 5
-FREEZE_TIME = 10
 
-lives = 3
 eggs_collected = 0
+golden_egg_hide_timer = 0
+freeze = 0
+lives = 3
+
 game_over = False
 game_complete = False
 reset_required = False
+golden_egg_collided = False
+golden_egg_hidden = False
+freeze_stop = False
+secret_pass= False
+up_mouse_counter = 0
+down_mouse_counter = 0
+press_mouse_counter = 0
 
+hero = Actor("hero", pos= HERO_START)
+golden_egg = Actor("golden-egg", pos= (WIDTH - 100, HEIGHT / 2))
 
 def create_lair(difficulty, dragon_pos, eggs_pos, egg_count, sleep_range):
     if difficulty == "hard-2":
-        amount_of_eggs = 0
-        egg_hidden = True
-        sleep_length = random.randint(sleep_range[0], sleep_range[1])
-    elif difficulty == "hard-3":
         amount_of_eggs = 0
         egg_hidden = True
         sleep_length = random.choice([0.1, 0.2, 0.3, 0.4, 0.5])
@@ -51,173 +63,75 @@ def create_lair(difficulty, dragon_pos, eggs_pos, egg_count, sleep_range):
             "wake_counter": 0
         }
 
-easy_lair_2 = {
-    "dragon": Actor("dragon-asleep", pos=(600, 160)),
-    "eggs": Actor("1-eggs", pos=(450, 200)),
-    "egg_count": 1,
-    "egg_hidden": False,
-    "egg_hide_counter": 0,
-    "sleep_length": random.randint(7, 10),
-    "sleep_counter": 0,
-    "wake_counter": 0
-}
-
-easy_lair_3 = {
-    "dragon": Actor("dragon-asleep", pos=(600, 560)),
-    "eggs": Actor("1-eggs", pos=(450, 100)),
-    "egg_count": 1,
-    "egg_hidden": False,
-    "egg_hide_counter": 0,
-    "sleep_length": random.randint(7, 10),
-    "sleep_counter": 0,
-    "wake_counter": 0
-}
-
-medium_lair_2 = {
-    "dragon": Actor("dragon-asleep", pos=(600, 360)),
-    "eggs": Actor("2-eggs", pos=(450, 400)),
-    "egg_count": 2,
-    "egg_hidden": False,
-    "egg_hide_counter": 0,
-    "sleep_length": random.randint(4, 7),
-    "sleep_counter": 0,
-    "wake_counter": 0
-}
-
-hard_lair_2 = {
-    "dragon": Actor("dragon-asleep", pos=(1000, 60)),
-    "eggs": Actor("3-eggs", pos=(450, 700)),
-    "egg_count": 0,
-    "egg_hidden": False,
-    "egg_hide_counter": 0,
-    "sleep_length": random.randint(1, 4),
-    "sleep_counter": 0,
-    "wake_counter": 0
-}
-
-hard_lair_3 = {
-    "dragon": Actor("dragon-asleep", pos=(1000, 160)),
-    "eggs": Actor("3-eggs", pos=(450, 700)),
-    "egg_count": 0,
-    "egg_hidden": False,
-    "egg_hide_counter": 0,
-    "sleep_length": random.randint(1, 4),
-    "sleep_counter": 0,
-    "wake_counter": 0
-}
-
-hard_lair_4 = {
-    "dragon": Actor("dragon-asleep", pos=(1000, 260)),
-    "eggs": Actor("3-eggs", pos=(450, 700)),
-    "egg_count": 0,
-    "egg_hidden": False,
-    "egg_hide_counter": 0,
-    "sleep_length": random.randint(1, 4),
-    "sleep_counter": 0,
-    "wake_counter": 0
-}
-
-hard_lair_5 = {
-    "dragon": Actor("dragon-asleep", pos=(1000, 360)),
-    "eggs": Actor("3-eggs", pos=(450, 700)),
-    "egg_count": 0,
-    "egg_hidden": False,
-    "egg_hide_counter": 0,
-    "sleep_length": random.randint(1, 4),
-    "sleep_counter": 0,
-    "wake_counter": 0
-}
-
-hard_lair_6 = {
-    "dragon": Actor("dragon-asleep", pos=(1000, 460)),
-    "eggs": Actor("3-eggs", pos=(450, 700)),
-    "egg_count": 0,
-    "egg_hidden": False,
-    "egg_hide_counter": 0,
-    "sleep_length": random.randint(1, 4),
-    "sleep_counter": 0,
-    "wake_counter": 0
-}
-
-hard_lair_7 = {
-    "dragon": Actor("dragon-asleep", pos=(1000, 560)),
-    "eggs": Actor("3-eggs", pos=(450, 700)),
-    "egg_count": 0,
-    "egg_hidden": False,
-    "egg_hide_counter": 0,
-    "sleep_length": random.randint(1, 4),
-    "sleep_counter": 0,
-    "wake_counter": 0
-}
-
-hard_lair_8 = {
-    "dragon": Actor("dragon-asleep", pos=(1400, 60)),
-    "eggs": Actor("3-eggs", pos=(450, 700)),
-    "egg_count": 0,
-    "egg_hidden": False,
-    "egg_hide_counter": 0,
-    "sleep_length": random.randint(1, 2),
-    "sleep_counter": 0,
-    "wake_counter": 0
-}
-
-hard_lair_9 = {
-    "dragon": Actor("dragon-asleep", pos=(1400, 160)),
-    "eggs": Actor("3-eggs", pos=(450, 700)),
-    "egg_count": 0,
-    "egg_hidden": False,
-    "egg_hide_counter": 0,
-    "sleep_length": random.randint(1, 2),
-    "sleep_counter": 0,
-    "wake_counter": 0
-}
-
-# list of dragon lairs
 lairs = [
     create_lair("easy", (600, 60), (450, 100), 1, (7, 10)), create_lair("medium", (600, 260), (450, 300), 2, (4, 7)), 
-    create_lair("hard", (600, 460), (600, 460), 3, (1, 4)), medium_lair_2, 
-    easy_lair_2, easy_lair_3, 
-    hard_lair_2, hard_lair_3,
-    hard_lair_4, hard_lair_5, 
-    hard_lair_6, hard_lair_7,
-    hard_lair_8, hard_lair_9,
-    create_lair("hard-2", (1400, 260), (450, 700), 3, (1, 2)), create_lair("hard-2", (1400, 360), (450, 700), 3, (1, 2)),
-    create_lair("hard-2", (1400, 460), (450, 700), 3, (1, 2)), create_lair("hard-2", (1400, 560), (450, 700), 3, (1, 2)),
-    create_lair("hard-2", (1700, 60), (450, 700), 3, (1, 2)), create_lair("hard-2", (1700, 160), (450, 700), 3, (1, 2)),
-    create_lair("hard-2", (1700, 260), (450, 700), 3, (1, 2)), create_lair("hard-2", (1700, 360), (450, 700), 3, (1, 2)),
-    create_lair("hard-2", (1700, 460), (450, 700), 3, (1, 2)), create_lair("hard-2", (1700, 560), (450, 700), 3, (1, 2)),
-    create_lair("hard-3", (2000, 60), (450, 700), 3, (1, 2)), create_lair("hard-3", (2000, 160), (450, 700), 3, (1, 2)),
-    create_lair("hard-3", (2000, 260), (450, 700), 3, (1, 2)), create_lair("hard-3", (2000, 360), (450, 700), 3, (1, 2)),
-    create_lair("hard-3", (2000, 460), (450, 700), 3, (1, 2)), create_lair("hard-3", (2000, 560), (450, 700), 3, (1, 2))
+    create_lair("hard", (600, 460), (450, 460), 3, (1, 4)), create_lair("medium", (600, 360), (450, 400), 2, (4, 7)), 
+    create_lair("easy", (600, 160), (450, 200), 1, (7, 10)), create_lair("easy", (600, 560), (450, 100), 1, (7, 10)), 
+    create_lair("hard", (1000, 60), (450, 700), 3, (1, 4)), create_lair("hard", (1000, 160), (450, 700), 3, (1, 4)),
+    create_lair("hard", (1000, 260), (450, 700), 3, (1, 4)), create_lair("hard", (1000, 360), (450, 700), 3, (1, 4)),
+    create_lair("hard", (1000, 460), (450, 700), 3, (1, 4)), create_lair("hard", (1000, 560), (450, 700), 3, (1, 4)), 
+    create_lair("hard", (1400, 60), (450, 700), 3, (1, 2)), create_lair("hard", (1400, 160), (450, 700), 3, (1, 2)), 
+    create_lair("hard", (1400, 260), (450, 700), 3, (1, 2)), create_lair("hard", (1400, 360), (450, 700), 3, (1, 2)),
+    create_lair("hard", (1400, 460), (450, 700), 3, (1, 2)), create_lair("hard", (1400, 560), (450, 700), 3, (1, 2)),
 ]
 
-hero = Actor("hero", pos= HERO_START)
-golden_egg = Actor("golden-egg", pos= (WIDTH - 100, HEIGHT / 2))
-golden_egg_collided = False
-golden_egg_hidden = False
-freeze = 0
-freeze_stop = False
+lairs_2 = [create_lair("hard", (300, 60), (450, 700), 3, (1, 2)), create_lair("hard", (300, 160), (450, 700), 3, (1, 2)),
+    create_lair("hard", (300, 260), (450, 700), 3, (1, 2)), create_lair("hard", (300, 360), (450, 700), 3, (1, 2)),
+    create_lair("hard", (300, 460), (450, 700), 3, (1, 2)), create_lair("hard", (300, 560), (450, 700), 3, (1, 2)),
+    create_lair("hard-2", (600, 60), (450, 700), 3, (1, 2)), create_lair("hard-2", (600, 160), (450, 700), 3, (1, 2)),
+    create_lair("hard-2", (600, 260), (450, 700), 3, (1, 2)), create_lair("hard-2", (600, 360), (450, 700), 3, (1, 2)),
+    create_lair("hard-2", (600, 460), (450, 700), 3, (1, 2)), create_lair("hard-2", (600, 560), (450, 700), 3, (1, 2))]
 
 def draw():
-    global lairs, eggs_collected, lives, game_complete, golden_egg_collided, freeze, freeze_stop
-    screen.blit("dungeon", (0, 0))
-    screen.blit("dungeon", (800, 0))
-    screen.blit("dungeon", (1600, 0))
+    global lairs, eggs_collected, lives, game_complete, golden_egg_collided, freeze, freeze_stop, golden_egg_hide_timer, golden_egg_hidden
+    draw_background()
     if game_over:
-        screen.draw.text("GAME OVER", fontsize= 60, center= CENTER, color= FONT_COLOR)
+        screen.draw.text("GAME OVER", fontsize=60, center=(CENTER_X - 400, CENTER_Y), color=FONT_COLOR)
+        screen.draw.text("GAME OVER", fontsize=60, center=(CENTER_X + 400, CENTER_Y), color=FONT_COLOR)
     elif game_complete:
-        screen.draw.text("YOU WON", fontsize= 60, center= CENTER, color= FONT_COLOR)
+        screen.draw.text("YOU WON", fontsize=60, center=(CENTER_X - 400, CENTER_Y), color=FONT_COLOR)
+        screen.draw.text("YOU WON", fontsize=60, center=(CENTER_X + 400, CENTER_Y), color=FONT_COLOR)
     else:
         hero.draw()
         draw_lairs(lairs)
         draw_counters(eggs_collected, lives)
         if not golden_egg_hidden:
             golden_egg.draw()
-        if freeze > FREEZE_TIME:
-            screen.draw.text(f"DRAGONS FROZEN for {FREEZE_TIME - freeze + 12}. GO!", fontsize= 60, center= (CENTER_X - 400, CENTER_Y), color= FONT_COLOR)
-            screen.draw.text(f"DRAGONS FROZEN for {FREEZE_TIME - freeze + 12}. GO!", fontsize= 60, center= (CENTER_X + 400, CENTER_Y), color= FONT_COLOR)
-        if freeze == FREEZE_TIME * 2 + 1:
-            freeze = 0
+        handle_freeze()
+        handle_golden_egg()
+
+def draw_background():
+    screen.blit("dungeon", (0, 0))
+    screen.blit("dungeon", (800, 0))
+    screen.blit("dungeon", (1600, 0))
+    if secret_pass:
+        screen.blit("dungeon", (0, 600))
+        screen.blit("dungeon", (800, 600))
+        screen.blit("dungeon", (1600, 600))
+
+def handle_freeze():
+    global freeze
+    if freeze > FREEZE_TIME:
+        screen.draw.text(f"DRAGONS FROZEN for {FREEZE_TIME - freeze + 12}. GO!", fontsize=60, center=(CENTER_X - 400, CENTER_Y), color=FONT_COLOR)
+        screen.draw.text(f"DRGONS FROZEN for {FREEZE_TIME - freeze + 12}. GO!", fontsize=60, center=(CENTER_X + 400, CENTER_Y), color=FONT_COLOR)
+    if freeze == FREEZE_TIME * 2 + 1:
+        freeze = 0
+
+def handle_golden_egg():
+    global golden_egg_hide_timer, golden_egg_hidden
+
+    if not golden_egg_hidden:
+        if golden_egg_hide_timer < GOLDEN_EGG_HIDE_TIME:
+            screen.draw.text(f"Golden Egg will hide in {GOLDEN_EGG_HIDE_TIME - golden_egg_hide_timer} seconds. GO!", fontsize=60, topleft=(10, 10), color=FONT_COLOR)
+        else:
+            golden_egg_hidden = True
+            golden_egg_hide_timer = 0
+    else:
+        if golden_egg_hide_timer < GOLDEN_EGG_HIDE_TIME:
+            screen.draw.text(f"Golden Egg will appear in {GOLDEN_EGG_HIDE_TIME - golden_egg_hide_timer} seconds. GO!", fontsize=60, topleft=(10, 10), color=FONT_COLOR)
+        else:
+            golden_egg_hidden = False
+            golden_egg_hide_timer = 0
 
 def draw_lairs(lairs_to_draw):
     for lair in lairs_to_draw:
@@ -230,30 +144,6 @@ def draw_counters(eggs_collected, lives):
     screen.draw.text(str(eggs_collected) + " / " + str(EGG_TARGET), fontsize = 40, pos=(30, HEIGHT - 50), color= FONT_COLOR)
     screen.blit("life-count", (125, HEIGHT -50))
     screen.draw.text(str(lives), fontsize = 40, pos=(155, HEIGHT - 50), color= FONT_COLOR)
-
-def update():
-    global lives
-    if keyboard.right:
-        hero.x += MOVE_DISTANCE
-        if hero.x > WIDTH:
-            hero.x = WIDTH
-    elif keyboard.left:
-        hero.x -= MOVE_DISTANCE
-        if hero.x < 0:
-            hero.x = 0
-    elif keyboard.down:
-        hero.y += MOVE_DISTANCE
-        if hero.y > HEIGHT:
-            hero.y = HEIGHT
-    elif keyboard.up:
-        hero.y -= MOVE_DISTANCE
-        if hero.y < 0:
-            hero.y = 0
-    if keyboard.a:
-        lives += 1
-    elif keyboard.d:
-        lives -= 1
-    check_for_collisions()
 
 def check_for_collisions():
     global lairs, eggs_collected, lives, reset_required, game_complete
@@ -297,12 +187,13 @@ def check_for_golden_egg_collision():
     if hero.colliderect(golden_egg):
         golden_egg_collided = True
         animate(hero, pos= HERO_START, duration= 0.01)
-        golden_egg_hidden =  True
+        golden_egg_hidden = True
+        eggs_collected += 100
         if eggs_collected >= EGG_TARGET:
             game_complete = True
 
 def update_lairs():
-    global lairs, hero, lives, freeze, freeze_stop
+    global lairs, hero, lives, freeze, freeze_stop, golden_egg_hidden, golden_egg_hide_timer
     for lair in lairs:
         if lair["dragon"].image == "dragon-asleep":
             update_sleeping_dragon(lair)
@@ -311,6 +202,10 @@ def update_lairs():
         update_egg(lair)
     if not freeze_stop:
         freeze += 1
+
+def update_golden_egg_timer():
+    global golden_egg_hide_timer
+    golden_egg_hide_timer += 1
 
 def update_sleeping_dragon(lair):
     if freeze < FREEZE_TIME:
@@ -340,6 +235,66 @@ def update_egg(lair):
         else:
             lair["egg_hide_counter"] += 1
 
+def update():
+    global lives
+    if keyboard.right:
+        hero.x += HERO_SPEED
+        if hero.x > WIDTH:
+            hero.x = WIDTH
+    elif keyboard.left:
+        hero.x -= HERO_SPEED
+        if hero.x < 0:
+            hero.x = 0
+    elif keyboard.down:
+        hero.y += HERO_SPEED
+        if hero.y > HEIGHT:
+            hero.y = HEIGHT
+    elif keyboard.up:
+        hero.y -= HERO_SPEED
+        if hero.y < 0:
+            hero.y = 0
+    if keyboard.a:
+        lives += 1
+    elif keyboard.d:
+        lives -= 1
+    check_for_collisions()
+    if hero.x > 1500:
+        hero.x = 0
+        move_screen()
+
+def on_mouse_down(pos, button):
+    global HEIGHT, secret_pass, up_mouse_counter, down_mouse_counter, press_mouse_counter
+    if not secret_pass:
+        if up_mouse_counter == 20 and down_mouse_counter == 20 and press_mouse_counter == 20:
+            if hero.collidepoint(pos):
+                HEIGHT = 1000
+                secret_pass = True
+            up_mouse_counter = 0
+            down_mouse_counter = 0
+            press_mouse_counter = 0
+    else:
+        if hero.collidepoint(pos):
+            if up_mouse_counter == 20 and down_mouse_counter == 20 and press_mouse_counter == 20:
+                HEIGHT = 1000
+                secret_pass = True
+            up_mouse_counter = 0
+            down_mouse_counter = 0
+            press_mouse_counter = 0
+    if button == 3:
+        if press_mouse_counter <= 20:
+            press_mouse_counter += 1
+    elif button == 4:
+        if up_mouse_counter <= 20:
+            up_mouse_counter += 1
+    elif button == 5:
+        if down_mouse_counter <= 20:
+            down_mouse_counter += 1
+
+def move_screen():
+    global lairs, lairs_2
+    lairs = lairs_2
+
 clock.schedule_interval(update_lairs, 1)
+clock.schedule_interval(update_golden_egg_timer, 1)
 
 pgzrun.go()
