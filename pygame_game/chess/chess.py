26573e414464b0ec5d4217d964b506ae7b8b7fd3
gootyboy <gootyboy@icloud.com>
2024-08-19 09:35:06 -0400

    jackfruit added

--- pygame_game/chess/chess.py
+++ pygame_game/chess/chess.py
@@ -12,6 +12,10 @@ WHITE_PAWN_Y = HEIGHT - (BOX_SIZE * 3 / 2) - OUTER_EDGE + 5
 BLACK_PAWN_Y = (BOX_SIZE * 5 / 2) - OUTER_EDGE
 WHITE_ROOK_Y = HEIGHT - (BOX_SIZE / 2) - OUTER_EDGE + 10
 BLACK_ROOK_Y = (BOX_SIZE / 2) + OUTER_EDGE - 10
+WHITE_QUEEN_Y = HEIGHT - (BOX_SIZE / 2) - OUTER_EDGE + 10
+BLACK_QUEEN_Y = (BOX_SIZE / 2) + OUTER_EDGE - 10
+WHITE_KING_Y = HEIGHT - (BOX_SIZE / 2) - OUTER_EDGE + 10
+BLACK_KING_Y = (BOX_SIZE / 2) + OUTER_EDGE - 10
 
 CIRCLE_RADIUS = 75
 
@@ -27,13 +31,35 @@ def create_piece(num, color, name):
         return Actor(color + "_" + name, (OUTER_EDGE + (BOX_SIZE * (num - 1)) + (BOX_SIZE / 2), BLACK_PAWN_Y if (color == "black") != colors_reversed else WHITE_PAWN_Y))
     elif name == "rook":
         return Actor(color + "_" + name, (OUTER_EDGE + (0 if num == 1 else BOX_SIZE * 7) + (BOX_SIZE / 2), BLACK_ROOK_Y if (color == "black") != colors_reversed else WHITE_ROOK_Y))
+    elif name == "knight":
+        return Actor(color + "_" + name, (OUTER_EDGE + (BOX_SIZE * (1 if num == 1 else 6)) + (BOX_SIZE / 2), BLACK_ROOK_Y if (color == "black") != colors_reversed else WHITE_ROOK_Y))
+    elif name == "bishop":
+        return Actor(color + "_" + name, (OUTER_EDGE + (BOX_SIZE * (2 if num == 1 else 5)) + (BOX_SIZE / 2), BLACK_ROOK_Y if (color == "black") != colors_reversed else WHITE_ROOK_Y))
+    elif name == "queen":
+        return Actor(color + "_" + name, (OUTER_EDGE + (BOX_SIZE * 3.5), BLACK_QUEEN_Y if (color == "black") != colors_reversed else WHITE_QUEEN_Y))
+    elif name == "king":
+        return Actor(color + "_" + name, (OUTER_EDGE + (BOX_SIZE * 4.5), BLACK_KING_Y if (color == "black") != colors_reversed else WHITE_KING_Y))
 
 def create_chess_pieces():
     pieces = {f"{color}_pawn_{num}": create_piece(num, color, "pawn") for color in ["white", "black"] for num in range(1, 9)}
-    pieces["white_rook_1"] = create_piece(1, "white", "rook")
-    pieces["white_rook_2"] = create_piece(2, "white", "rook")
-    pieces["black_rook_1"] = create_piece(1, "black", "rook")
-    pieces["black_rook_2"] = create_piece(2, "black", "rook")
+    pieces.update({
+        "white_rook_1": create_piece(1, "white", "rook"),
+        "white_rook_2": create_piece(2, "white", "rook"),
+        "black_rook_1": create_piece(1, "black", "rook"),
+        "black_rook_2": create_piece(2, "black", "rook"),
+        "white_knight_1": create_piece(1, "white", "knight"),
+        "white_knight_2": create_piece(2, "white", "knight"),
+        "black_knight_1": create_piece(1, "black", "knight"),
+        "black_knight_2": create_piece(2, "black", "knight"),
+        "white_bishop_1": create_piece(1, "white", "bishop"),
+        "white_bishop_2": create_piece(2, "white", "bishop"),
+        "black_bishop_1": create_piece(1, "black", "bishop"),
+        "black_bishop_2": create_piece(2, "black", "bishop"),
+        "white_queen": create_piece(1, "white", "queen"),
+        "black_queen": create_piece(1, "black", "queen"),
+        "white_king": create_piece(1, "white", "king"),
+        "black_king": create_piece(1, "black", "king")
+    })
     return pieces
 
 def set_up_board():
@@ -59,8 +85,8 @@ def update():
     pass
 
 def is_path_clear(piece, y):
-    for other_piece in pieces.values():
-        if other_piece != piece and other_piece.collidepoint((piece.x, y)):
+    for p in pieces.values():
+        if p != piece and p.collidepoint((piece.x, y)):
             return False
     return True
 
@@ -87,7 +113,7 @@ def check_for_piece_collision(pos):
             if "pawn" in name and OUTER_EDGE + BOX_SIZE - 10 < piece.y < HEIGHT - OUTER_EDGE - BOX_SIZE + 10:
                 if is_path_clear(piece, new_y):
                     piece.y = new_y
-            elif "rook" in name:
+            elif "rook" in name or "knight" in name or "bishop" in name or "queen" in name or "king" in name:
                 if is_path_clear(piece, new_y):
                     piece.y = new_y
 
